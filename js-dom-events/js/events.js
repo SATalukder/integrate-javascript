9f7efaf1dc0ca7497da8f60d18031e843f2e1a03
SATalukder <shajibahmad152@gmail.com>
2023-02-23 01:11:14 +0600

    Use events handler

--- js-dom-events/js/events.js
+++ js-dom-events/js/events.js
@@ -1,1 +1,30 @@
+
+//option 1: directly set on the HTML elements
+
 console.log('This is separate JS file');
+
+//option 2 :add onclike function
+
+function redMake() {
+    document.body.style.backgroundColor = 'red';
+
+}
+
+//option: 3
+
+const makeBlueButton = document.getElementById('make-blue');
+// console.log(makeBlueButton);
+makeBlueButton.onclick = blueMake;
+function blueMake() {
+    document.body.style.backgroundColor = 'blue';
+
+}
+
+//option 3: another
+
+const makepurpleButton = document.getElementById('make-purple');
+// console.log(makeBlueButton);
+makepurpleButton.onclick = function purpleMake() {
+    document.body.style.backgroundColor = 'purple';
+
+}
